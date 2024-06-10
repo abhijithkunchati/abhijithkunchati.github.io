3829db029b6e8f27a76c31549eec0d8b4ac3ad13
Abhijith <abhijithkunchati@gmail.com>
2024-06-09 19:52:48 -0700

    Changed some tect

--- language.js
+++ language.js
@@ -62,13 +62,13 @@ var data = {
 
     "help" : `<li><code>about-me</code>: Display information about me</li>
     <li><code>clear</code>: Clean the terminal</li>
-    <li><code>experiences</code>: Displays the list of my experiences</li>
-    <li><code>get cv</code>: Download my CV</li>
+    <li><code>experiences</code>: Displays a list of my experiences</li>
+    <li><code>get cv</code>: Opens my CV/Resume</li>
     <li><code>get linkedin</code>: Link to my Linkedin</li>
     <li><code>get github</code>: Link to my Github</li>
     <li><code>help</code>: Displays the list of commands</li>
-    <li><code>passions</code>: Displays the list of my hobbies</li>
-    <li><code>my-projects</code>: Displays the list of my personal projects</li>
+    <li><code>passions</code>: Displays a list of my hobbies</li>
+    <li><code>my-projects</code>: Displays a list of my personal projects</li>
     <li><code>themes</code>: Change the terminal theme</li>
     <li><em>You can use the TAB key to complete a command line</em></li>
     <li><em>You can find the old commands with the up and down arrows</em></li>`,
